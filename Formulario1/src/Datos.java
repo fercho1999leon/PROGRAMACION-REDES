1e7d2a01bab4a20f08fe594b5a682f0f9b5312b9
suleyma <suleyma@DESKTOP-DMSA5VQ>
2020-12-16 14:09:27 -0500

    VERIFICAR1

--- Formulario1/src/Datos.java
+++ Formulario1/src/Datos.java
@@ -15,7 +15,7 @@ public class Datos extends javax.swing.JFrame {
     public Datos() {
         initComponents();
     }
-
+    //VERIFICAR1
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
