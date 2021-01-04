8fa1ded83cb4c1fa2ca6e965968e58a202448e49
FERNANDO <FERNANDO@DESKTOP-Q0T2DUU>
2021-01-04 11:48:18 -0500

    INTERFAZ GUI

--- INTERFAZ-GUI-04-01-2021/src/interfaz/gui/pkg04/pkg01/pkg2021/INTERFAZGUI04012021.java
+++ INTERFAZ-GUI-04-01-2021/src/interfaz/gui/pkg04/pkg01/pkg2021/INTERFAZGUI04012021.java
@@ -16,6 +16,7 @@ public class INTERFAZGUI04012021 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("BIENVENIDO");
     }
     
 }
