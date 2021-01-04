902a0f1bf8a3fd216a34b8c1db2f599b65b0e30e
FERNANDO <FERNANDO@DESKTOP-Q0T2DUU>
2021-01-04 11:36:12 -0500

    INTERFAZ GUI

---
+++ INTERFAZ-GUI-04-01-2021/src/interfaz/gui/pkg04/pkg01/pkg2021/INTERFAZGUI04012021.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaz.gui.pkg04.pkg01.pkg2021;
+
+/**
+ *
+ * @author FERNANDO
+ */
+public class INTERFAZGUI04012021 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
